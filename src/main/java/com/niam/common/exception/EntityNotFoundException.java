dc1a0331ab5ed66f1ec25e530c16f37169fd782b
r.naby <r.naby@behsacorp.com>
2025-11-11 13:30:18 +0330

    Initial Commit

--- src/main/java/com/niam/common/exception/EntityNotFoundException.java
+++ src/main/java/com/niam/common/exception/EntityNotFoundException.java
@@ -4,4 +4,8 @@ public class EntityNotFoundException extends BusinessException {
     public EntityNotFoundException(Integer responseCode, Integer reasonCode, String responseDescription) {
         super(responseCode, reasonCode, responseDescription);
     }
+
+    public EntityNotFoundException(String responseDescription) {
+        super(400, 400, responseDescription);
+    }
 }
--- src/main/java/com/niam/common/exception/IllegalStateException.java
+++ src/main/java/com/niam/common/exception/IllegalStateException.java
@@ -4,4 +4,8 @@ public class IllegalStateException extends BusinessException {
     public IllegalStateException(Integer responseCode, Integer reasonCode, String responseDescription) {
         super(responseCode, reasonCode, responseDescription);
     }
+
+    public IllegalStateException(String responseDescription) {
+        super(400, 400, responseDescription);
+    }
 }
