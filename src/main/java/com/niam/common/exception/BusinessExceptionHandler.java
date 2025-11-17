c91a76889b1ed8efe22ea897d21fa1031ad6d54e
r.naby <r.naby@behsacorp.com>
2025-11-17 13:07:14 +0330

    Initial Commit

--- src/main/java/com/niam/common/exception/BusinessExceptionHandler.java
+++ src/main/java/com/niam/common/exception/BusinessExceptionHandler.java
@@ -1,48 +1,35 @@
 package com.niam.common.exception;
 
 import com.niam.common.model.response.ErrorResponse;
-import com.niam.common.utils.MessageUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class BusinessExceptionHandler extends ResponseEntityExceptionHandler {
-    private final MessageUtil messageUtil;
-
-    public BusinessExceptionHandler(MessageUtil messageUtil) {
-        this.messageUtil = messageUtil;
-    }
-
-    @ExceptionHandler({BusinessException.class})
-    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e, HandlerMethod handlerMethod) {
-        logger.error(e.getMessage(), e);
-        if (handlerMethod.getMethod().getReturnType() == byte[].class) {
-            ErrorResponse errorResponse = ErrorResponse.builder()
-                    .responseCode(e.getResponseCode())
-                    .reasonCode(e.getReasonCode())
-                    .responseDescription(e.getResponseDescription())
-                    .build();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .body(errorResponse);
-        }
-        return null;
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e, HttpServletRequest request) {
+        ErrorResponse body = ErrorResponse.builder()
+                .responseCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .reasonCode(HttpStatus.INTERNAL_SERVER_ERROR.series().value())
+                .responseDescription("Business error")
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(body);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        logger.error(e.getMessage(), e);
-        return ResponseEntity.internalServerError().body(
-                ErrorResponse.builder()
-                        .responseCode(500)
-                        .reasonCode(500)
-                        .responseDescription(e.getMessage())
-                        .build()
-        );
+    public ResponseEntity<ErrorResponse> handleException(Exception e, HttpServletRequest request) {
+        ErrorResponse body = ErrorResponse.builder()
+                .responseCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .reasonCode(HttpStatus.INTERNAL_SERVER_ERROR.series().value())
+                .responseDescription(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .build();
+        return ResponseEntity.status(500).body(body);
     }
 }
