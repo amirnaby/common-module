b85515115cda423103ab352025e82b025f116037
r.naby <r.naby@behsacorp.com>
2025-11-18 12:24:38 +0330

    Initial Commit

--- src/main/java/com/niam/common/config/LogbackConfig.java
+++ src/main/java/com/niam/common/config/LogbackConfig.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogbackConfig {
-
     @Value("${common.logging-file.enable}")
     private boolean enableFileLogging;
 
---
+++ src/main/java/com/niam/common/utils/GenericDtoMapper.java
@@ -0,0 +1,35 @@
+package com.niam.common.utils;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenericDtoMapper {
+
+    public static <T> void copyNonNullProperties(Object source, T target, String... ignoreFields) {
+        List<String> ignoreList = Arrays.asList(ignoreFields);
+
+        for (Field field : source.getClass().getDeclaredFields()) {
+            if (ignoreList.contains(field.getName()))
+                continue;
+
+            try {
+                field.setAccessible(true);
+                Object value = field.get(source);
+                if (value != null) {
+                    Field targetField = null;
+                    try {
+                        targetField = target.getClass().getDeclaredField(field.getName());
+                    } catch (NoSuchFieldException ignored) {}
+
+                    if (targetField != null) {
+                        targetField.setAccessible(true);
+                        targetField.set(target, value);
+                    }
+                }
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
